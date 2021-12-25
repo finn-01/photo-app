004799462a8b284f69a926e6f66b680f60099826
finn-01 <nguyensam576@gmail.com>
2021-12-25 23:01:30 +0700

    fix start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./index.css";
+import React from "react";
+import Title from "./comps/Title";
 
 function App() {
-	return <div className="App">Hello World</div>;
+	return (
+		<div className="App">
+			<Title />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Title() {
+	return (
+		<div className="title">
+			<h1>Firegame</h1>
+			<h2>Your Picture</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		</div>
+	);
+}
+
+export default Title;
